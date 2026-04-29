df81b8d39dc897f9183ab99af8de677c3be48fc7
jeampylisuko5-ops <jeampylisuko5@gmail.com>
2026-04-29 12:06:46 +0100

    Create general.py

---
+++ general.py
@@ -0,0 +1,9 @@
+import os
+
+print("Bot montreal-bound prêt à décoller depuis Kinshasa 🇨🇩✈️🇨🇦")
+
+# Ton code principal ira ici
+# Si t'as un token Discord :
+# import discord
+# client = discord.Client()
+# client.run(os.getenv("TOKEN"))
